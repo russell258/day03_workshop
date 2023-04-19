a9a0dffdd2e244fe5d2520d98734cd40c64c14c8
russell258 <duckie258@gmail.com>
2023-04-20 01:28:45 +0800

    added add and save

--- src/main/java/sg/edu/nus/iss/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/sg/edu/nus/iss/App.java
@@ -1,4 +1,4 @@
-package sg.edu.nus.iss;
+package sg.edu.nus.iss.sg.edu.nus.iss;
 
 import java.io.Console;
 import java.io.File;
@@ -56,9 +56,13 @@ public class App
                 File loginFile = new File(dirPath + File.separator +loginuser);
                 if (loginFile.exists()){
                     System.out.println("File " + loginuser + " already exists");
+                    
+                    //need to print out what is in their cart into cartItems
+
                 }else{
                     //login File is the directory Path "dirPath" joined with "loginuser"
                     loginFile.createNewFile();
+                    System.out.println(loginuser + ", your cart is empty");
                 }
             }
             if (input.startsWith("users")){
@@ -72,24 +76,54 @@ public class App
             }
 
             if (input.startsWith("add")){
-                input = input.replace(',',' ');
+            input = input.replace(',',' ');
+            Scanner scan = new Scanner (input.substring(4));
+            String content ="";
+            while (scan.hasNext()){
+                content = scan.next();
+                if (!cartItems.contains(content)){
+                    cartItems.add(content);
+                    System.out.println(content + " added to cart");
+                }else{
+                    System.out.println(loginuser + ", "+content + " already in cart");
+                }
+            }
+        }
 
+            if (input.equals("list")){
+                int i = 0;
+                for (String item: cartItems){
+                    i++;
+                    System.out.println(i + ". " + item);
+                }
+            }
+            if (input.startsWith("delete")){
+                Scanner scan = new Scanner(input.substring(6));
+                String content = "";
+                while (scan.hasNext()){
+                    content = scan.next();
+                    int listIndex = Integer.parseInt(content);
+
+                    if (listIndex <cartItems.size()){
+                        cartItems.remove(listIndex);
+                    } else {
+                        System.err.println("Incorrect item index");
+                    }
+                }
+            }
+                
+
+            if (input.startsWith("save")){
                 // 1.use FileWriter & PrintWriter to write to a <loginuser> file
                 // instantiate the objects 
                 FileWriter fw = new FileWriter(dirPath + File.separator +loginuser, false);
                 PrintWriter pw = new PrintWriter(fw);
 
-                String currentScanString = "";
-                Scanner inputScanner = new Scanner(input.substring(4));
-                while(inputScanner.hasNext()){
-                    currentScanString=inputScanner.next();
-                    cartItems.add(currentScanString);
-                    
-                    //2. write to file using PW
-                    pw.write("\n" + currentScanString);
-                    
+                for (String item:cartItems){
+                    pw.print("\n" + item);
                 }
-
+                System.out.println("\nYour cart has been saved");
+                
                 //flush and close FW and PW objects after
                 pw.flush();
                 pw.close();
